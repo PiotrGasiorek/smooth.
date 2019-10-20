8b400bdc18a32c3cb78e2180cd5ccb9297666558
Piotr Gąsiorek <35469939+PiotrGasiorek@users.noreply.github.com>
2019-10-20 21:05:44 +0200

    Added function called toggleButtons()
    Added function called toggleButtons() which blocks scrolling until animation is finished.

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 window.onload = slideEffect();
 
 function slideEffect(){
-    console.log('started');
+    function toggleButtons(toggle){
+        for(button of buttons){
+            button.style.pointerEvents = toggle;
+        }
+    }
     const animationSlider = document.getElementsByClassName('animationSlider')[0];
+    const buttons = document.getElementsByClassName('btn');
+    // Block buttons until animation is finished
+    toggleButtons('none');
     animationSlider.classList.add('loadingScreen');
     setTimeout(function(){
+        // Unblock buttons
+        toggleButtons('');
         animationSlider.classList.remove('loadingScreen');
     }, 900);
-
 };
